061fdb7669b11240be893f4bbf639fb6df5b35a9
ReaperNode-ops <ReaperNode@icloud.com>
2026-06-02 18:47:42 -0400

    Add auroChat function for chat API interaction

---
+++ src/auroAI.js
@@ -0,0 +1,10 @@
+export async function auroChat(messages) {
+  const res = await fetch("/api/chat", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ messages })
+  });
+
+  const data = await res.json();
+  return data.response;
+}
